8966214081787bbafee59743dcce7143b138a7b3
nguyenhoangxz7@gmail.com <nguyenhoangxz7@gmail.com>
2025-01-07 11:12:59 +0700

    update_stat

--- src/main/java/code/repository/CategoryRepository.java
+++ src/main/java/code/repository/CategoryRepository.java
@@ -12,17 +12,16 @@ public interface CategoryRepository extends JpaRepository<Category,Long> {
   Optional<Category> findById(long category_id) ;
   Optional<Category> findByName(String category_name) ;
 
-  @Query(value = """
-    SELECT COALESCE(SUM(rent_count), 0) AS total_rent_count
-    FROM (
-        SELECT COUNT(od.id) AS rent_count
-        FROM product p
-        JOIN product_detail pd ON p.id = pd.product_id
-        JOIN order_detail od ON pd.id = od.product_detail_id
-        WHERE p.category_id = :categoryId AND od.status = 8
-        GROUP BY p.id
-    ) AS product_rent_counts
-    """, nativeQuery = true)
+  @Query("""
+    SELECT COALESCE(SUM(COUNT(od)), 0)
+    FROM Category c
+    JOIN c.products p
+    JOIN p.productDetails pd
+    JOIN OrderDetail od ON pd.id = od.productDetail.id
+    WHERE c.id = :categoryId AND od.status = 8
+    GROUP BY c.id
+    """)
   Long calculateTotalRentCountForCategory(@Param("categoryId") Long categoryId);
+
 
 }
