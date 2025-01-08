b159fd4f991b87009564d1c445f1c1e3231b08bc
Tên của bạn <nguyenhoangxz7@gmail.com>
2025-01-08 11:07:08 +0700

    a

--- src/main/java/code/controller/admin/test.java
+++ src/main/java/code/controller/admin/test.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class test {
-  @GetMapping("/api/admin/test")
-  public ResponseEntity<?> get(
-      @AuthenticationPrincipal CustomUserDetails userDetails
-  ){
-    return ResponseEntity.ok(userDetails.getUser());
-  }
+//  @GetMapping("/api/admin/test")
+//  public ResponseEntity<?> get(
+//      @AuthenticationPrincipal CustomUserDetails userDetails
+//  ){
+//    return ResponseEntity.ok(userDetails.getUser());
+//  }
 
   @GetMapping("/api/customer/test")
   public ResponseEntity<?>  getCustomer(
